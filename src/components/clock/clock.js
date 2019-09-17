d9561565b6e328e27f68f634d359312604c56517
tala <tala@Tala-2.local>
2019-09-17 21:29:25 +0800

    cleanup

--- src/components/clock/clock.js
+++ src/components/clock/clock.js
@@ -4,80 +4,64 @@ import './style.css';
 export default class Clock extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
+        seconds: 0,
+        minutes: 0,
+        hours: 0,
         secondDegrees: {},
         minuteDegrees: {},
         hourDegrees: {}
     }
-
-    this.seconds = 0;
-    this.minutes = 0;
-    this.hours   =  0; 
-    this.clockInterval = null;
-
-    this.updateClock    = this.updateClock.bind(this);
-    this.setSecondHand  = this.setSecondHand.bind(this);
-    this.setMinuteHand  = this.setMinuteHand.bind(this);
-   
   }
 
-  updateClock(time) {
-
-    this.seconds = time.getSeconds();
-    this.minutes = time.getMinutes();
-    this.hours = time.getHours();
+  updateClock = (time) =>  {
+    this.setState({
+      seconds: time.getSeconds(),
+      minutes: time.getMinutes(),
+      hours: time.getHours()
+    })
     
     this.setSecondHand();
     this.setMinuteHand();
     this.setHourHand();
   }
 
-  setSecondHand() {
-    const degrees = (this.seconds / 60 * 360) + 90;
+  setSecondHand = () => {
+    const degrees = (this.state.seconds / 60 * 360) + 90;
     this.setState({
         secondDegrees : { transform : `rotate(${degrees}deg)`} 
     })
   }
 
-  setMinuteHand() {
-    const degrees = (( this.minutes + this.seconds / 60 ) / 60 * 360) + 90;
+  setMinuteHand = () => {
+    const degrees = (( this.state.minutes + this.state.seconds / 60 ) / 60 * 360) + 90;
     this.setState({
         minuteDegrees : { transform : `rotate(${degrees}deg)`} 
     })
   }
 
-  setHourHand() {
-    const degrees = (( this.hours + this.minutes / 60 + this.seconds / 3600 ) / 12 * 360) + 90;
+  setHourHand = () => {
+    const degrees = (( this.state.hours + this.state.minutes / 60 + this.state.seconds / 3600 ) / 12 * 360) + 90;
     this.setState({
         hourDegrees : { transform : `rotate(${degrees}deg)`} 
     })
   }
 
   componentDidMount() {
-    if(this.clockInterval===null){
-      this.updateClock( new Date() );
-      this.clockInterval = setInterval( () => this.updateClock( new Date ), 1000 );
-    }
-  }
-
-  componentWillUnmount() {
-    this.clockInterval = null;
+      setInterval( () => this.updateClock( new Date ), 1000 );
   }
 
   render() {
     return (
-        <div ref={this.element}>
-            <div id="clock" ref={this.clock}>
-                <div className="top"></div>
-                <div className="right"></div>
-                <div className="bottom"></div>
-                <div className="left"></div>
-                <div id='second-hand' className='hand' style={this.state.secondDegrees}></div>
-                <div id='minute-hand' className='hand' style={this.state.minuteDegrees}></div>
-                <div id='hour-hand' className='hand' style={this.state.hourDegrees}></div>
-            </div>
-        </div>
+      <div id="clock" ref={this.clock}>
+          <div className="top"></div>
+          <div className="right"></div>
+          <div className="bottom"></div>
+          <div className="left"></div>
+          <div id='second-hand' className='hand' style={this.state.secondDegrees}></div>
+          <div id='minute-hand' className='hand' style={this.state.minuteDegrees}></div>
+          <div id='hour-hand' className='hand' style={this.state.hourDegrees}></div>
+      </div>
     )
   }
 }
